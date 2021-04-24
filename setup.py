b29b6406e2b1a370f542b8f301ed6891c518daff
Quoc <huynhquoc1990@gmail.com>
2021-04-25 02:26:43 +0700

    V01

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     version='1.0.0',
     packages=setup.find_packages(where="src"),
     url='https://github.com/huynhquoc1990/Test_Load.git',
+    project_urls={
+        "Bug Tracker": "https://github.com/huynhquoc1990/Test_Load.git",
+    },
     license='MIT',
     author='Quoc',
     author_email='huynhquoc1990@gmail.com',
@@ -17,6 +20,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+
     package_dir={"": "src"},
     python_requires=">=3.6"
 )
