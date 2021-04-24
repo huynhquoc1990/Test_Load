e6c48549b73e69c707d3f9415e81df683a1c006d
Quoc <huynhquoc1990@gmail.com>
2021-04-25 02:17:42 +0700

    V01

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup
-
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 setup(
     name='Test_Load',
     version='1.0.0',
-    packages=['Test_Load'],
+    packages=setup.find_packages(where="src"),
     url='https://github.com/huynhquoc1990/Test_Load.git',
     license='MIT',
     author='Quoc',
     author_email='huynhquoc1990@gmail.com',
-    description='Demo Load Code To Pypi'
+    description='Demo Load Code To Pypi',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    python_requires=">=3.6"
 )
