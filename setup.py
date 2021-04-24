f56a702ca18419b6bd1541435fcea64c8d6b5ddf
Quoc <huynhquoc1990@gmail.com>
2021-04-25 02:08:06 +0700

    V01

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Test_Load',
+    version='1.0.0',
+    packages=['Test_Load'],
+    url='https://github.com/huynhquoc1990/Test_Load.git',
+    license='MIT',
+    author='Quoc',
+    author_email='huynhquoc1990@gmail.com',
+    description='Demo Load Code To Pypi'
+)
